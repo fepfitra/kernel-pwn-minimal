ca958f9a859f0d12373654c7f675fd5cc1400926
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2026-01-01 00:39:16 +0700

    idk

--- pack.sh
+++ pack.sh
@@ -14,9 +14,10 @@ fi
 
 if [ -d linux-5.4 ] && [ ! -f linux-5.4/include/config/auto.conf ]; then
     echo "[*] Preparing kernel headers..."
-    make -C linux-5.4 modules_prepare > /dev/null 2>&1
+    yes "" | make -C linux-5.4 modules_prepare > /dev/null 2>&1
     if [ ! -f linux-5.4/include/config/auto.conf ]; then
         echo "[-] Error: Failed to prepare kernel headers."
+        yes "" | make -C linux-5.4 modules_prepare
         exit 1
     fi
 fi
